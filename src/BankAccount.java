05137f8223e0c03e82e8398007bae28808bc2f23
Adit-Mugdha-Das <mailbox.mugdha@gmail.com>
2025-09-19 22:51:37 +0600

    backaccount.java added

---
+++ src/BankAccount.java
@@ -0,0 +1,24 @@
+public class BankAccount {
+    private String accountNumber;
+    private String accountHolderName;
+    private double balance;
+
+    public BankAccount(String accountNumber, String accountHolderName, double openingBalance) {
+        this.accountNumber = accountNumber;
+        this.accountHolderName = accountHolderName;
+        this.balance = openingBalance;
+    }
+
+    public void deposit(double amount) {
+        if (amount > 0) balance += amount;
+    }
+
+    public boolean withdraw(double amount) {
+        if (amount > 0 && amount <= balance) { balance -= amount; return true; }
+        return false;
+    }
+
+    public double checkBalance() {
+        return balance;
+    }
+}
